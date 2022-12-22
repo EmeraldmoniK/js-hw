8dedb011a133911f7eaf00e17b547c5557769f44
JSONe <23@.ru>
2022-12-22 19:33:39 +0500

    SUPERJS

---
+++ main.js
@@ -0,0 +1,26 @@
+var usrName = prompt("Введите свое имя")
+console.log("Ваше имя " + usrName);
+var age = prompt("Введите свой возраст")
+console.log("Ваш возраст " + age);
+
+
+
+var plus = prompt("6 + 3 = ")
+alert("6 + 3 =" +(6 + 3)+ ", ваш ответ:" + plus)
+console.log("6 + 3 =" +(6 + 3)+ ", ваш ответ:" + plus);
+
+var min = prompt("6 - 3 = ")
+alert("6 - 3 =" +(6 - 3)+ ", ваш ответ:" + min)
+console.log("6 - 3 =" +(6 - 3)+ ", ваш ответ:" + min);
+
+var add = prompt("6 * 3 = ")
+alert("6 * 3 =" +(6 * 3)+ ", ваш ответ:" + add)
+console.log("6 * 3 =" +(6 * 3)+ ", ваш ответ:" + add);
+
+var div = prompt("6 / 3 = ")
+alert("6 / 3 =" +(6 / 3)+ ", ваш ответ:" + div)
+console.log("6 / 3 =" +(6 / 3)+ ", ваш ответ:" + div);
+
+var pr = prompt("6 % 3 = ")
+alert("6 % 3 =" +(6 % 3)+ ", ваш ответ:" + pr)
+console.log("6 % 3 =" +(6 % 3)+ ", ваш ответ:" + pr);
